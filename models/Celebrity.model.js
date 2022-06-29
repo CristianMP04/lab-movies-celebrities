a070c902b577300928dd9030bcdd0e6c97999894
CristianMP04 <christian.marti.pernin@gmail.com>
2022-06-29 10:23:36 +0200

    update
    update

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,17 @@
-//  Add your code here
+const mongoose = require("mongoose");
+
+const celibritySchema = new mongoose.Schema({
+    name: {
+      type: String
+    },
+    occupation: {
+      type: String
+    },
+    catchPhrase: {
+      type: [String]
+    }
+});
+
+const Celebrity = mongoose.model("Celebrity", celibritySchema);
+//Pokemon -> pokemons
+module.exports = Celebrity;
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,33 @@
-// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+
+
+const Celebrity = require("../models/Celebrity.model");
+
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 
 // all your routes here
 
-module.exports = router;
+
+router.get("/celebrities/create", (req, res) => {
+    res.render("celebrities/new-celebrity.hbs");
+});
+
+router.post("celebrities/create", (req, res, next) => {
+    const {name, occupation, catchPhrase } = req.params;
+    Celebrity.create({name, occupation, catchPhrase})
+    .then(() => res.redirect('/celebrities'))
+    .catch(error => res.redirect('celebrities/new-celebrity'));
+  });
+
+  
+  router.get("/celebrities/celebrities", (req, res) => {
+          Celebrity
+          .find()
+          .then(celebrityFromApi => {
+                  console.log(celebrityFromApi)
+                res.render("../views/celebrities/celebrities.hbs", { celebrityFromApi });
+              })
+              .catch(error => console.log(error));
+            });
+            
+          module.exports = router;
